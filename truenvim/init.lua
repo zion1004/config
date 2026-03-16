f1e68b54f5a7f0274cc733b0df9d65bb7e97266b
zion1004 <bicoljoy12@gmail.com>
2026-03-17 00:26:49 +0900

    actual nvim config

---
+++ truenvim/init.lua
@@ -0,0 +1,8 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffffff", bg = "none" })
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.scrolloff = 20
